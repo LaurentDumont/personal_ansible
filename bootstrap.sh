94de94609dff55773855eb88d615401179203a8c
Laurent Dumont <LaurentDumont@users.noreply.github.com>
2017-03-17 20:14:21 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/usr/bin/echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" > /etc/apt/sources.list.d/ansible.list
+/bin/echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" > /etc/apt/sources.list.d/ansible.list
 /usr/bin/apt-get update
 /usr/bin/apt-get install ansible
 /usr/local/bin/ansible-playbook -i ansible/host ansible/configure_machine.yml --ask-pass --ask-become-pass
