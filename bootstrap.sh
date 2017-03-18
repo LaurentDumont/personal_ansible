732de7087c1720c732c8f6af02e75f6d2cd140a2
LaurentDumont <ldumont@coldnorthadmin.com>
2017-03-17 20:18:40 -0400

    Bootstrap script is now functionning

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 /bin/echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" > /etc/apt/sources.list.d/ansible.list
+/usr/bin/apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 /usr/bin/apt-get update
-/usr/bin/apt-get install ansible
-/usr/local/bin/ansible-playbook -i ansible/host ansible/configure_machine.yml --ask-pass --ask-become-pass
+/usr/bin/apt-get install ansible -y -qq
+/usr/bin/ansible-playbook -i ansible/host ansible/configure_machine.yml --ask-pass --ask-become-pass
